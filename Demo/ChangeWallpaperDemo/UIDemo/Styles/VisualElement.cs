6e87271f9fffd946916adf67df2921e77e8b4b6f
Memoyu <mmy6076@outlook.com>
2021-06-13 02:03:24 +0800

    fix:完善首页壁纸预览列表展示

---
+++ Demo/ChangeWallpaperDemo/UIDemo/Styles/VisualElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace UIDemo.Styles
+{
+    public class VisualElement
+    {
+        public static readonly DependencyProperty HighlightBrushProperty = DependencyProperty.RegisterAttached(
+            "HighlightBrush", typeof(Brush), typeof(VisualElement), new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetHighlightBrush(DependencyObject element, Brush value)
+            => element.SetValue(HighlightBrushProperty, value);
+
+        public static Brush GetHighlightBrush(DependencyObject element)
+            => (Brush)element.GetValue(HighlightBrushProperty);
+    }
+}
